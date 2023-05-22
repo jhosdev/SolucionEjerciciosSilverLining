14e17fa65dba2b6eadae5559e8da5cbbf4785de3
Jhosaim <86936147+jhosR@users.noreply.github.com>
2023-05-22 10:55:23 -0500

    lastest

--- EjercicioGo.go
+++ EjercicioGo.go
@@ -1,22 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func esPar(numero int) bool {
-    if numero%2 == 0 {
-        return true
-    }
-    return false
+	if numero%2 == 0 {
+		return true
+	}
+	return false
 }
 
 func main() {
-    // Ejemplo de uso de funciones
-    //genera un numero aleatorio
-    numero := rand.Intn(100)
-    
-    if esPar(numero) {
-        fmt.Println(numero, "es par")
-    } else {
-        fmt.Println(numero, "es impar")
-    }
+	// Ejemplo de uso de funciones
+	//genera un numero aleatorio
+	//generate a random number
+	numero := rand.Intn(100)
+
+	if esPar(numero) {
+		fmt.Println(numero, "es par")
+	} else {
+		fmt.Println(numero, "es impar")
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jhosdev/SolucionEjerciciosSilverLining
+
+go 1.20
